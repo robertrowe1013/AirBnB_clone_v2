8042476b3c209963218a041a9ff2897fbdca8380
Sean Taylor <silverwolf4350@gmail.com>
2020-08-13 19:10:53 +0000

    Made State.cities dynamic to storage type.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """ Returns a list of cities related to self """
+            citylist = storage.all(City)
+            related = []
+
+            for key in citylist.keys():
+                if citylist[key].state_id == self.id:
+                    related.append(citylist[key])
+
+            return related

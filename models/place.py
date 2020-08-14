dc1f54b1c7771f95657b0a2fa0ffa5edceeae1fc
Sean Taylor <silverwolf4350@gmail.com>
2020-08-14 02:22:45 +0000

    Added many to many relationship between Amenity and Place.

--- models/place.py
+++ models/place.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models import storage
+from sqlalchemy import Table, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+
+place_amenity = Table('place_amenity', Base.metadata, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False), Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel):
@@ -16,3 +23,21 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            """ Represents all related amenities. """
+            amens = storage.all(Amenity)
+            related = []
+            for key in amens.keys():
+                if amens[key].id in amenity_ids:
+                    related.append(amens[key])
+            return related
+
+        @amenities.setter
+        def amenities(self, obj):
+            if isinstance(obj, Amenity):
+                amenity_ids.append(obj.id)

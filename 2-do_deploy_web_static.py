433012a50488d0e6b4eb88392e13646713c0996d
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-18 04:36:49 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,20 +4,6 @@ from fabric.api import *
 import time
 import subprocess
 import os
-
-
-def do_pack():
-    """ compress files """
-    try:
-        now = time.strftime("%Y%m%d%H%M%S")
-        filename = "versions/web_static_" + now + ".tgz"
-        if not os.path.isdir("versions"):
-            os.makedirs("versions")
-        bash_cmd = local("tar -cvzf {} web_static".format(filename))
-        return ("$filename")
-    except:
-        return None
-
 env.hosts = ['34.75.53.84', '3.89.225.162']
 
 

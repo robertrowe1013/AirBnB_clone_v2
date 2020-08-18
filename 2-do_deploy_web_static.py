74af544c0090e91b44976082db91e049b1dec50f
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-18 04:25:19 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,7 +23,6 @@ env.hosts = ['34.75.53.84', '3.89.225.162']
 
 def do_deploy(archive_path):
     """ decompress files after sending """
-
     try:
         if not os.path.exists(archive_path):
             return False
@@ -31,6 +30,7 @@ def do_deploy(archive_path):
         filename = archive_path[9:-4]
         arch_dest = "/data/web_static/releases/" + filename + "/"
         run("sudo mkdir -p {}".format(arch_dest))
+        run("sudo ls {}".format(arch_dest))
         run("sudo tar -xvzf {} {}".format(archive_path, arch_dest))
         run("sudo rm -R {}".format(archive_path))
         run("sudo ln -sf /data/web_static/releases/{}/ \

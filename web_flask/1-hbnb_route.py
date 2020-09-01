b8a5f7b40a90b6576fc9a999e40f895c94e53491
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-01 04:54:07 +0000

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,10 +5,16 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ hello message """
+    return 'Hello, HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     """ hello message """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)

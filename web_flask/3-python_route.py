cd5248584ebf44a74e9e4b082281bd941cf4361d
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-01 07:14:54 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" flask route """
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ hello message """
+    return 'Hello, HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ hello message """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def Ctext(text):
+    """ c is fun """
+    text = text.replace('_', ' ')
+    return 'C %s' % escape(text)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Pythontext(text='is_cool'):
+    """ python is magic """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
